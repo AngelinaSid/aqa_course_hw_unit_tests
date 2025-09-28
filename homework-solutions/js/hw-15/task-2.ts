79ec5713ae75eb8dbc2708c449cafe73a6a165b1
angelina.sidoruk <sidoruk.ang@gmail.com>
2025-09-28 21:08:34 +0300

    решение hw-15

---
+++ homework-solutions/js/hw-15/task-2.ts
@@ -0,0 +1,16 @@
+/* Напишите дженерик-функцию getKeyByValue, которая принимает объект и значение, и возвращает ключ, соответствующий этому значению. 
+Если значение не найдено, функция должна возвращать undefined.
+Используйте keyof для типизации ключей объекта
+*/
+
+function getKeyByValue<T extends object>(
+    obj: T,
+    value: T[keyof T]
+): keyof T | undefined {
+    for(const key in obj){
+        if (obj[key] === value){
+            return key as keyof T
+        }
+    }
+    return undefined
+}
